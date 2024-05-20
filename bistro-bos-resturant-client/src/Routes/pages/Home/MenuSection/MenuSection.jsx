cd07a9e4a9c3c749cf300901629671a805d1f838
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-05-21 01:15:32 +0600

    Menu section added

--- bistro-bos-resturant-client/src/Routes/pages/Home/MenuSection/MenuSection.jsx
+++ bistro-bos-resturant-client/src/Routes/pages/Home/MenuSection/MenuSection.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+import MenuItem from "../../../../Shared/MenuItem/MenuItem";
 import SectionTitle from "../../../../components/SectionTitle";
+import { Link } from "react-router-dom";
 
 const MenuSection = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+        fetch('/menu.json')
+            .then(res => res.json())
+            .then(data => {
+                // const popularItems = data.filter(item => item.category === 'popular');
+                setMenu(data)
+            })
+    }, [])
     return (
-        <div>
-            <SectionTitle heading={'FROM OUR MENU'} message={'Check it out'} />
-        </div>
+        <section className="w-11/12 md:w-4/5 mx-auto max-w-[1500px] py-14">
+            <SectionTitle
+                message="Check it out"
+                heading="FROM OUR MENU"
+            ></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                    menu.slice(0,6).map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+            <div className="flex items-center justify-center py-12">
+                <Link className="">
+                    <button className="px-6 mx-auto py-3 rounded-lg hover:bg-base-200 duration-150 text-[20px] font-medium border-0 border-b-[#1F2937] border-b-4">View Full Menu</button>
+                </Link>
+            </div>
+        </section>
     );
 };
 
---
+++ bistro-bos-resturant-client/src/Routes/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../../components/SectionTitle";
+import MenuItem from "../../../../Shared/MenuItem/MenuItem";
+
+const PopularMenu = () => {
+   
+};
+
+export default PopularMenu;
