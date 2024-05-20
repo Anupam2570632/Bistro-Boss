84fd87bdac0ba114e515262b7085e0f9f696fde6
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-05-20 23:32:01 +0600

    assets and banner added

--- bistro-bos-resturant-client/src/Shared/NavBar/NavBar.jsx
+++ bistro-bos-resturant-client/src/Shared/NavBar/NavBar.jsx
@@ -10,18 +10,18 @@ const NavBar = () => {
         <li><NavLink to={'/shops'}>OUR SHOPS</NavLink></li>
     </>
     return (
-        <div>
-            <div className="navbar bg-black bg-opacity-40">
+        <div className="fixed top-0 z-10 w-full m-0 p-0 bg-black bg-opacity-40">
+            <div className="navbar max-w-[1550px] mx-auto ">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost text-white lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className=" dropdown-content mt-3 z-[1] p-2  w-52">
+                        <ul tabIndex={0} className=" dropdown-content bg-black bg-opacity-45 mt-3 z-[1] p-2  w-52">
                             {links}
                         </ul>
                     </div>
-                    <Link to={'/'} className="flex flex-col text-white">
+                    <Link to={'/'} className="flex flex-col p-0 text-white">
                         <h2 className="text-[18px] font-bold">BISTRO BOSS</h2>
                         <h1 className="tracking-widest">RESTAURANT</h1>
                     </Link>
